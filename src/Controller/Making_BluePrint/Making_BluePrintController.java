bbb2008b00b13ab0320067d928afdc62678bba39
Blizzard03 <mariq.rasyid@gmail.com>
2022-10-19 00:06:47 +0700

    Update README.md

---
+++ src/Controller/Making_BluePrint/Making_BluePrintController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Controller.Making_BluePrint;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author mariq
+ */
+public class Making_BluePrintController implements Initializable {
+//final ObservableList option;
+
+@FXML
+ComboBox CPU;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
